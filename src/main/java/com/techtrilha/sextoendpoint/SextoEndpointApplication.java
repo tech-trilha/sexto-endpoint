d7990007af40e985d334c7ebc9ec4f92e8f45a94
Felipe Miranda <felip.simi@gmail.com>
2020-06-27 00:29:16 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/techtrilha/sextoendpoint/SextoEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.techtrilha.sextoendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SextoEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SextoEndpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtrilha/sextoendpoint/SextoEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtrilha.sextoendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SextoEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
